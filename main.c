c723ae5d2c93383bdb012b33a6fa541abc1be402
ravidkrisi <ravidkrisi22@gmail.com>
2022-11-24 14:34:54 +0000

    updated main for correct outpout version 1.2

--- main.c
+++ main.c
@@ -8,22 +8,21 @@ int main(){
 
     printf("The Armstrong numbers are:");
     for(int i=x; i<=y;i++){
-        if(isArmstrong(i)==1) printf("%d ",i);
+        if(isArmstrong(i)==1) printf(" %d",i);
     }
 
     printf("\nThe Palindromes are:");
     for(int i=x; i<=y;i++){
-        if(isPalindrome(i)==1) printf("%d ",i);
+        if(isPalindrome(i)==1) printf(" %d",i);
     }
 
     printf("\nThe Prime numbers are:");
     for(int i=x; i<=y;i++){
-        if(isPrime(i)==1) printf("%d ",i);
+        if(isPrime(i)==1) printf(" %d",i);
     }
 
     printf("\nThe Strong numbers are:");
     for(int i=x; i<=y;i++){
-        if(isStrong(i)==1) printf("%d ",i);
+        if(isStrong(i)==1) printf(" %d",i);
     }
-    printf("\n");
 }
